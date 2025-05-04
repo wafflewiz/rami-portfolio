2b287b53dd939a32fa9ce7d0d21403b531996983
Rami Aljanaby <ramialj94@yahoo.com>
2025-05-04 18:21:03 -0500

    updated outDir again

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +9,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      outDir: "dist"
     },
   },
-})
+  build: {
+    outDir: "dist", // Ensure the output directory is correctly set
+  },
+});
